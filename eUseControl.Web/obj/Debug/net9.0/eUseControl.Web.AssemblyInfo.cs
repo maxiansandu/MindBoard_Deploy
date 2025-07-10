f6eca21daf21ceb427d3e961bdeb8463291acf6d
maxiansandu <maxian.sandu@gmail.com>
2025-07-11 02:25:00 +0300

    update Dockerfile

--- eUseControl.Web/obj/Debug/net9.0/eUseControl.Web.AssemblyInfo.cs
+++ eUseControl.Web/obj/Debug/net9.0/eUseControl.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eUseControl.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567aeb2a18b46127c0cfa32cb92a943b2ee59237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4b36554733fdd16323cd3bc2cf0d2a129579c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("eUseControl.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eUseControl.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
